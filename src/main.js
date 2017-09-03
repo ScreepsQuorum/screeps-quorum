cb7329c7ce2a5e6a03384ef7b012bd4cacb7058a
Robert Hafner <tedivm@tedivm.com>
2017-09-02 17:11:00 -0700

    put it all together

---
+++ src/main.js
@@ -0,0 +1,32 @@
+'use strict'
+
+if(Game.cpu.bucket < 500) {
+  throw new Error('Extremely low bucket - aborting script run at top level')
+}
+
+
+// Load any prototypes or libraries
+
+/* Get Upload Version */
+require('version')
+
+/* Enable QOS Logger */
+var QosLogger = require('qos_logger')
+global.Logger = new QosLogger()
+
+/* Add "sos library" - https://github.com/ScreepsOS/sos-library */
+global.SOS_LIB_PREFIX = 'thirdparty_'
+global.sos_lib = require('thirdparty_sos_lib')
+
+/* Add "creep talk" library - https://github.com/screepers/creeptalk */
+var language = require('thirdparty_creeptalk_emoji')
+require('thirdparty_creeptalk')({'public': true, 'language': language})
+
+var QosKernel = require('qos_kernel')
+
+module.exports.loop = function () {
+  var kernal = new QosKernel()
+  kernal.start()
+  kernal.run()
+  kernal.shutdown()
+}
