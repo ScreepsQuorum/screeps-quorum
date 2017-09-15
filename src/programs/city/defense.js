a05c6757ad20796ab643c78c59fadbc9579ee92a
Robert Hafner <tedivm@tedivm.com>
2017-09-15 00:17:20 -0700

    Split tower filling duties to a dedicated role
    This adds an argument to `Room.prototype.getStructuresToFill` for
    structure types. It then adds a constructor based property to the
    `Filler` role, making it easier to split off new roles that only fill
    specific structures.

--- src/programs/city/defense.js
+++ src/programs/city/defense.js
@@ -25,6 +25,10 @@ class CityDefense extends kernel.process {
       this.fireTowers(towers, hostiles)
     }
 
+    if (towers && _.some(towers, (tower) => tower.energy < tower.energyCapacity)) {
+      this.launchCreepProcess('loader', 'replenisher', this.data.room, 1)
+    }
+
     const playerHostiles = hostiles.filter(c => c.owner.username !== 'Invader')
     if (playerHostiles.length > 0) {
       Logger.log(`Hostile creep owned by ${c.owner.username} detected in room ${this.data.room}.`, LOG_WARN)
---
+++ src/roles/replenisher.js
@@ -0,0 +1,10 @@
+const Filler = require('roles_filler')
+
+class Replenisher extends Filler {
+  constructor () {
+    super()
+    this.fillableStructures = [STRUCTURE_TOWER]
+  }
+}
+
+module.exports = Replenisher
