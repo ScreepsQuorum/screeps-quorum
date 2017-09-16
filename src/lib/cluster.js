97976e628166acd760b6b42adf947e7d6ef2257c
Robert Hafner <tedivm@tedivm.com>
2017-09-16 00:20:13 -0700

    Add Cluster library, another way to manage creeps

---
+++ src/lib/cluster.js
@@ -0,0 +1,69 @@
+'use strict'
+
+class Cluster {
+  static clean () {
+    var clusters = Cluster.listAll()
+    for (var clustername of clusters) {
+      var cluster = new Cluster(clustername)
+      if (Memory.clusters[clustername].creeps.length <= 0) {
+        delete Memory.clusters[clustername]
+      }
+    }
+  }
+
+  static listAll () {
+    if (!Memory.clusters) {
+      return []
+    }
+    return Object.keys(Memory.clusters)
+  }
+
+  constructor (name, room = false) {
+    this.name = name
+    this.room = room
+    if (!Memory.clusters) {
+      Memory.clusters = {}
+    }
+    if (!Memory.clusters[name]) {
+      Memory.clusters[name] = {
+        'creeps': []
+      }
+    }
+    this.memory = Memory.clusters[name]
+    this.creeps = []
+    for (var creepname of this.memory.creeps) {
+      if (Game.creeps[creepname]) {
+        this.creeps.push(Game.creeps[creepname])
+        continue
+      }
+      if (room) {
+        if (!room.isQueued(creepname)) {
+          this.memory.creeps.indexOf.splice(this.memory.creeps.indexOf(creepname), 1)
+        }
+      } else if (!Room.isQueued(creepname)) {
+        this.memory.creeps.indexOf.splice(this.memory.creeps.indexOf(creepname), 1)
+      }
+    }
+  }
+
+  sizeCluster (role, quantity, options = {}, room = false) {
+    if (this.memory.creeps.length >= quantity) {
+      return true
+    }
+    var spawnroom = room || this.room
+    while (this.memory.creeps.length < quantity) {
+      this.memory.creeps.push(spawnroom.queueCreep(role, options))
+    }
+    return false
+  }
+
+  getCreeps () {
+    return this.creeps
+  }
+
+  forEach (creepAction) {
+    return this.getCreeps().forEach(creepAction)
+  }
+}
+
+module.exports = Cluster
