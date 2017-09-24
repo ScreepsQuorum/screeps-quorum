3ae5d575b7dee414516508abf35b1e273b060178
Robert Hafner <tedivm@tedivm.com>
2017-09-24 00:50:39 -0700

    remove buggy getLinkPosition

--- src/extends/source.js
+++ src/extends/source.js
@@ -3,7 +3,3 @@
 Source.prototype.getMiningPosition = function () {
   return this.pos.getMostOpenNeighbor()
 }
-
-Source.prototype.getLinkPosition = function () {
-  return this.getMiningPosition().getMostOpenNeighbor()
-}
