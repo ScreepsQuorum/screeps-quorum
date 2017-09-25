061842e889c060ede6cbcf048753c37c41e67b4a
Robert Hafner <tedivm@tedivm.com>
2017-09-24 20:55:01 -0700

    Don't ever put work parts on replenisher

--- src/roles/replenisher.js
+++ src/roles/replenisher.js
@@ -5,8 +5,13 @@ const Filler = require('roles_filler')
 class Replenisher extends Filler {
   constructor () {
     super()
-    this.defaultEnergy = 1100
+    this.defaultEnergy = 800
     this.fillableStructures = [STRUCTURE_TOWER]
+  }
+
+  getBuild (room, options) {
+    this.setBuildDefaults(room, options)
+    return Creep.buildFromTemplate([MOVE, CARRY], options.energy)
   }
 }
 
