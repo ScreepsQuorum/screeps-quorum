6aef1ac08b235d8e5fca75e8c5caa9608de20fef
Robert Hafner <tedivm@tedivm.com>
2017-10-01 09:03:26 -0700

    fix math in comments

--- src/roles/fracker.js
+++ src/roles/fracker.js
@@ -5,10 +5,9 @@ const MetaRole = require('roles_meta')
 class Fracker extends MetaRole {
   getBuild (room, options) {
     this.setBuildDefaults(room, options)
-    // 10 per harvest * 0.2 harvests per tick = 2 mineral per tick
-    // 25 ticks to fill
+    // 10 per harvest * 0.166 harvests per tick = 1.66 mineral per tick
     // 10 ticks fatigue per move - 20 distance (estimated) == 220 ticks (without roads)
-    // 1280 ticks == 2560 minerals per 12 parts
+    // 1280 ticks == 2133 minerals per 12 parts
     // Extra move at 50 parts reduces fatigue to 7 ticks.
     const base = [MOVE, CARRY, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK]
     return Creep.buildFromTemplate(base, options.energy)
