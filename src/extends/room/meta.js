56ce3dfa84959e17931a013e8df66c472de8535e
Robert Hafner <tedivm@tedivm.com>
2017-10-06 00:05:30 -0700

    Room.getRoomsInRange, Room.isSourcekeeper

--- src/extends/room/meta.js
+++ src/extends/room/meta.js
@@ -51,3 +51,48 @@ Room.getCoordinates = function (name) {
     'y_dir': match[3]
   }
 }
+
+Room.getRoomsInRange = function (name, range) {
+  const coords = Room.getCoordinates(name)
+  const startXdir = coords.x_dir
+  const startYdir = coords.y_dir
+  const left = coords.x - range
+  const right = +coords.x + +range
+  const top = coords.y - range
+  const bottom = +coords.y + +range
+  const lowerX = Math.min(left, right)
+  const upperX = Math.max(left, right)
+  const lowerY = Math.min(top, bottom)
+  const upperY = Math.max(top, bottom)
+
+  let rooms = []
+  for (var x = lowerX; x <= upperX; x++) {
+    for (var y = lowerY; y <= upperY; y++) {
+      let xNorm, xdir
+      if (x < 0) {
+        xNorm = Math.abs(x) - 1
+        xdir = startXdir === 'E' ? 'W' : 'E'
+      } else {
+        xNorm = x
+        xdir = startXdir
+      }
+      let yNorm, ydir
+      if (y < 0) {
+        yNorm = Math.abs(y) - 1
+        ydir = startYdir === 'N' ? 'S' : 'N'
+      } else {
+        yNorm = y
+        ydir = startYdir
+      }
+      rooms.push(xdir + xNorm + ydir + yNorm)
+    }
+  }
+  return rooms
+}
+
+Room.isSourcekeeper = function (name) {
+  const coords = Room.getCoordinates(name)
+  let xMod = coords.x % 10
+  let yMod = coords.y % 10
+  return xMod >= 4 && xMod <= 6 && yMod >= 4 && yMod <= 6
+}
