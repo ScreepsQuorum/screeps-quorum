0d7941287cc102aa196ea73ed9ea69d712c023a1
Robert Hafner <tedivm@tedivm.com>
2017-10-11 00:21:49 -0700

    Refresh intel more often

--- src/programs/empire/intel.js
+++ src/programs/empire/intel.js
@@ -24,7 +24,7 @@ class EmpireIntel extends kernel.process {
         continue
       }
       const roominfo = room.getIntel(roomname)
-      if (Game.time - roominfo[INTEL_UPDATED] > 500) {
+      if (Game.time - roominfo[INTEL_UPDATED] > 60) {
         room.saveIntel()
       }
     }
