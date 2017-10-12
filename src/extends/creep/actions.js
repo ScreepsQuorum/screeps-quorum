e0e28391f0eb0d13ca3de8bd6b4e8c9890b5b13f
Robert Hafner <tedivm@tedivm.com>
2017-10-11 23:08:58 -0700

    return early if there are no active sources

--- src/extends/creep/actions.js
+++ src/extends/creep/actions.js
@@ -47,6 +47,11 @@ Creep.prototype.recharge = function () {
     return true
   }
   const sources = this.room.find(FIND_SOURCES_ACTIVE)
+  if (sources.length <= 0) {
+    // Still returning true since energy is still needed
+    return true
+  }
+
   sources.sort((a, b) => a.pos.getRangeTo(a.room.controller) - b.pos.getRangeTo(b.room.controller))
   const idx = parseInt(this.name[this.name.length - 1], 36)
   const source = sources[idx % sources.length]
