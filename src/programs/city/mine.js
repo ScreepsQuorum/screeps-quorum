9ae8d80bc655d6a4d8e4510e1b4f3d3378e1d1a5
Michael Overhorst <michael.overhorst@gmail.com>
2017-10-18 16:34:35 +0200

    The underlying problem seem to be found, typo in the extension name.

--- src/programs/city/mine.js
+++ src/programs/city/mine.js
@@ -110,11 +110,7 @@ class CityMine extends kernel.process {
     } else if (this.room.terminal) {
       storage = this.room.terminal
     } else if (this.remote) {
-      if (!this.room.structure) {
-        return
-      }
-
-      const containers = this.room.structure[STRUCTURE_CONTAINER]
+      const containers = this.room.structures[STRUCTURE_CONTAINER]
 
       if (containers && containers.length > 0) {
         if (containers.length > 1) {
@@ -122,7 +118,7 @@ class CityMine extends kernel.process {
         }
         storage = containers[0]
       } else {
-        const spawns = this.room.structure[STRUCTURE_SPAWN]
+        const spawns = this.room.structures[STRUCTURE_SPAWN]
         if (spawns) {
           if (spawns.length > 1) {
             spawns.sort((a, b) => a.energy - b.energy)
