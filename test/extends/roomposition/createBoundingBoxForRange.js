fd39afb92c0ae5702d134dd7cdda9f0f36fe792c
PostCrafter <dev@postcrafter.de>
2017-10-19 15:34:24 +0200

    Add some basic test cases

---
+++ test/extends/roomposition/createBoundingBoxForRange.js
@@ -0,0 +1,44 @@
+import test from 'ava'
+
+import '../../helpers/screeps-globals'
+
+import { createBoundingBoxForRange } from '../../../src/extends/roomposition'
+
+const BOUND_MIN = 0
+const BOUND_MAX = 49
+
+function testMacro (t, x, y, range) {
+  const { left, right, top, bottom } = createBoundingBoxForRange(x, y, range)
+
+  t.true(left <= right, 'Left value is less than or equal to right value')
+  t.true(top <= bottom, 'Bottom value is less than or equal to top value')
+
+  t.true(BOUND_MIN <= left && left <= BOUND_MAX, 'Left value is out of bound')
+  t.true(BOUND_MIN <= right && right <= BOUND_MAX, 'Right value is out of bound')
+  t.true(BOUND_MIN <= top && top <= BOUND_MAX, 'Top value is out of bound')
+  t.true(BOUND_MIN <= bottom && bottom <= BOUND_MAX, 'Bottom value is out of bound')
+
+  // @todo add some assertions based on range
+}
+
+test('Works for x great than y', testMacro, 20, 10, 10)
+
+test('Works for x less than y', testMacro, 10, 20, 10)
+
+test('Works for x equal to y', testMacro, 10, 10, 10)
+
+test('Works for negative range', testMacro, 10, 10, -10)
+
+test('Works for negative x value', testMacro, -10, 10, 10)
+
+test('Works for negative y value', testMacro, 10, -10, 10)
+
+test('Works for small out of bound x value', testMacro, -100, 10, 10)
+
+test('Works for large out of bound x value', testMacro, 100, 10, 10)
+
+test('Works for small out of bound y value', testMacro, 10, -100, 10)
+
+test('Works for large out of bound y value', testMacro, 10, 100, 10)
+
+test('Works for large range value', testMacro, 10, 10, 100)
