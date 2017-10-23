b4d763101f19d54b4cba8ceb7e69ad2c686c1790
TheRealMaxion <TheRealMaxion@users.noreply.github.com>
2017-10-23 07:08:30 +0200

    Plan roads within the room, resolves #92
    Automatic road planning from core to flowers, controller, sources and minerals.
    
    Completely untested as of yet.

--- src/programs/city/layout.js
+++ src/programs/city/layout.js
@@ -340,7 +340,9 @@ class CityLayout extends kernel.process {
 
     const flower2Adjusted = new RoomPosition(flower2Pos.x - LAYOUT_FLOWER_BUFFER, flower2Pos.y - LAYOUT_FLOWER_BUFFER, this.data.room)
     this.planStructureMatrix(layout, flower2Adjusted, LAYOUT_FLOWER, (2 * LAYOUT_FLOWER_BUFFER) + 1)
-
+    
+    this.planRoads(layout, corePos, flower1Pos, flower2Pos)
+    
     layout.save()
     Logger.log(`Room planning for room ${this.data.room} has successfully completed`)
     return this.suicide()
@@ -365,13 +367,106 @@ class CityLayout extends kernel.process {
         }
         const structure = matrix[row][column]
         layout.planStructureAt(structure, leftCorner.x + column, leftCorner.y + row)
-        if (structure !== STRUCTURE_ROAD && !OBSTACLE_OBJECT_TYPES[structure]) {
+        if (structure !== STRUCTURE_ROAD && !OBSTACLE_OBJECT_TYPES.contains(structure)) {
           layout.planStructureAt(STRUCTURE_ROAD, leftCorner.x + column, leftCorner.y + row)
         }
       }
     }
   }
+  
+  /**
+   * Plan roads from the core to the flowers and major room features (controller, sources, minerals).
+   */
+  planRoads (layout, corePos, flower1Pos, flower2Pos) {
+    let matrix = this.getConstructionMatrix(layout)
+    this.planRoad(layout, corePos, flower1Pos, matrix)
+    this.planRoad(layout, corePos, flower2Pos, matrix)
+    if (this.room.controller) {
+      this.planRoad(layout, corePos, this.room.controller.pos, matrix)
+    }
+    const sources = this.room.find(FIND_SOURCES)
+    let source
+    for (source of sources) {
+      this.planRoad(layout, corePos, source.pos, matrix)
+    }
+    const minerals = this.room.find(FIND_MINERALS)
+    let mineral
+    for (mineral of minerals) {
+      this.planRoad(layout, corePos, mineral.pos, matrix)
+    }
+  }
+  
+  /*
+   * Plan a road between the given two positions within the same room.
+   * If a costMatrix is given, that will be used instead of generating a new one from the given layout's current state.
+   */
+  planRoad (layout, fromPos, toPos, matrix) {
+    if (!matrix) matrix = this.getConstructionMatrix(layout)
+    let path = PathFinder.search(fromPos, {pos: toPos, range: 1}, {plainCost: 4, swampCost: 5, maxRooms: 1, maxOps: 6000, roomCallback: (roomName) => {if(roomName !== this.room.name) {return false} else {return matrix}}})
+    if (!path || path.incomplete) path = PathFinder.search(fromPos, {pos: toPos, range: 1}, {plainCost: 4, swampCost: 5, maxRooms: 1, maxOps: 6000, roomCallback: (roomName) => {if(roomName !== this.room.name) {return false} else {return matrix}}, heuristicWeight: 1.5})
+    let spot
+    for(spot of path.path) {
+      if (layout.planStructureAt(STRUCTURE_ROAD, spot.x, spot.y)) matrix.set(spot.x, spot.y, 1)
+    }
+  }
+  
+  /**
+   * Generate a road construction costMatrix from the given layout.
+   * Only structures in the layout are taken into consideration, unplanned existing structures are ignored. (maybe not ideal, discuss)
+   */
+  getConstructionMatrix (layout) {
+    const costMatrix = new PathFinder.CostMatrix()
+    let x,
+      y,
+      plannedStruct
+    for (x = 1; x < 49; ++x) {
+      for (y = 1; y < 49; ++y) {
+        const pos = new RoomPosition(x, y, this.data.room)
+        if (pos.inFrontOfExit()) {
+          costMatrix.set(x, y, 30)
+        }
+        if (layout) {
+          plannedStruct = layout.get(x, y)
+          if (plannedStruct === 3) { // magic number due to lacking access to structureMap
+            costMatrix.set(x, y, 1)
+          } else if (![0, 3, 5, 7, 16, 17].contains(plannedStruct)) { // magic numbers due to lacking access to structureMap
+            costMatrix.set(x, y, 0xff)
+          }
+        }
+      }
+    }
+    /**
+     * Ranges and costs for positions near controller and resources can be adjusted to taste in testing,
+     * these may not even be needed for just planning roads.
+     */
+    if (this.room.controller) {
+      const poses = this.room.controller.pos.getAdjacentInRange(2)
+      let pos
+      for (pos of poses) {
+        costMatrix.set(pos.x, pos.y, 10)
+      }
+    }
+    const sources = this.room.find(FIND_SOURCES)
+    let source,
+      pos
+    for (source of sources) {
+      const poses = source.pos.getAdjacentInRange(1)
+      for (pos of poses) {
+        costMatrix.set(pos.x, pos.y, 10)
+      }
+    }
+    const minerals = this.room.find(FIND_MINERALS)
+    let mineral
+    for (mineral of minerals) {
+      const poses = mineral.pos.getAdjacentInRange(1)
+      for (pos of poses) {
+        costMatrix.set(pos.x, pos.y, 10)
+      }
+    }
 
+    return costMatrix
+  }
+  
   /**
    * Return positions with the required amount of room around them. A random result from all possible matches is
    * returned, or if a sort function is provided it will be run *after* a shuffle is applied (so positions with the same
