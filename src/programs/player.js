6631ffcd765c57e0114e6b1ad4b048c21ae8be24
TheRealMaxion <TheRealMaxion@users.noreply.github.com>
2017-10-27 21:34:22 +0200

    Small fix to prevent some errors

--- src/programs/player.js
+++ src/programs/player.js
@@ -24,7 +24,7 @@ class Player extends kernel.process {
     let roomname
     for (roomname of cities) {
       /* Launch a "City" program for each city saved in memory. `Room.addCity` to add new rooms. */
-      if (Game.rooms[roomname].controller && Game.rooms[roomname].controller.my) {
+      if (Game.rooms[roomname] && Game.rooms[roomname].controller && Game.rooms[roomname].controller.my) {
         this.launchChildProcess(`room_${roomname}`, 'city', {
           'room': roomname
         })
