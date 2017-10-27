47e173955d6d14b89b3227d7f3e7a4e1d49f5af7
TheRealMaxion <TheRealMaxion@users.noreply.github.com>
2017-10-27 21:42:29 +0200

    Another small error fix

--- src/programs/player.js
+++ src/programs/player.js
@@ -46,7 +46,7 @@ class Player extends kernel.process {
       const defaultPriorityStats = sos.lib.monitor.getPriorityRunStats(PRIORITIES_CREEP_DEFAULT)
       if (defaultPriorityStats && defaultPriorityStats['long'] <= 1.25) {
         if (cities.length < Game.gcl.level) {
-          if (cities.length > 1 || Game.rooms[cities[0]].getRoomSetting('EXPAND_FROM')) {
+          if (cities.length > 1 || (Game.rooms[cities[0]] && Game.rooms[cities[0]].getRoomSetting('EXPAND_FROM'))) {
             this.launchChildProcess('expand', 'empire_expand')
           }
         }
