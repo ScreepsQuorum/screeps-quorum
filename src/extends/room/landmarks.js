4975baaa6116d2dcce78f5e6c944841d3631674b
screeps-automated-user <25467084+screeps-automated-user@users.noreply.github.com>
2017-11-28 18:27:04 +0100

    restructured logic in getSuicideBooth
    make sure there is at least one valid spawn in the room, set the first available spawn as default spawn, then check if there's a spawn near the storage

--- src/extends/room/landmarks.js
+++ src/extends/room/landmarks.js
@@ -1,15 +1,16 @@
 
 Room.prototype.getSuicideBooth = function () {
+  // Make sure there are spawns in the room
+  if (!this.structures[STRUCTURE_SPAWN] || !this.structures[STRUCTURE_SPAWN].length) {
+    return false
+  }
+  
+  // Defaults to the first spawn
+  let spawn = this.structures[STRUCTURE_SPAWN][0]
+  
   // Identify spawn closest to storage, to make reclaimed energy easier to store.
-  let spawn = false
   if (this.storage) {
     spawn = this.storage.pos.findClosestByRange(this.structures[STRUCTURE_SPAWN])
-  } else if (this.structures[STRUCTURE_SPAWN].length) {
-    spawn = this.structures[STRUCTURE_SPAWN][0]
-  }
-
-  if (!spawn) {
-    return false
   }
 
   // Pick the location immediately above the spawn and recycle there.
