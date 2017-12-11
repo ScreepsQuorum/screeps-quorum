2fdbd186321b6a8a6ec16132dda021a85b78360a
Leo Friedrichs <dev@postcrafter.de>
2017-12-11 14:09:49 +0100

    Use constant instead of magic value

--- src/programs/empire/expand.js
+++ src/programs/empire/expand.js
@@ -64,7 +64,7 @@ class EmpireExpand extends kernel.process {
     }
 
     // If layout isn't complete after a full generation unclaim and try again somewhere else.
-    if (!this.data.recover && Game.time - this.data.claimedAt > 1500) {
+    if (!this.data.recover && Game.time - this.data.claimedAt > CREEP_LIFE_TIME) {
       if (!this.colony.getLayout().isPlanned() && !this.isChildProcessRunning('layout')) {
         this.colony.controller.unclaim()
         delete this.data.claimedAt
