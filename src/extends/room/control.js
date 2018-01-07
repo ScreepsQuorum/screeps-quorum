70697c689ed5703c1fdd53d32c1ae3694ffab4be
Robert Hafner <tedivm@tedivm.com>
2018-01-07 10:40:18 -0800

    Revert "Increase number of remote mines per room"

--- src/extends/room/control.js
+++ src/extends/room/control.js
@@ -35,12 +35,11 @@ let roomLevelOptions = {
   7: {
     'RESERVER_COUNT': 1,
     'ALLOW_MINING_SCALEBACK': false,
-    'REMOTE_MINES': 3,
     'ALWAYS_SAFEMODE': true
   },
   8: {
     'UPGRADERS_QUANTITY': 1,
-    'REMOTE_MINES': 4,
+    'REMOTE_MINES': 3,
     'SHARE_ENERGY': true
   }
 }
