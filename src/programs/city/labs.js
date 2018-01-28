12decc407e16e8b909da9d171eb60eddcac65418
Robert Hafner <tedivm@tedivm.com>
2018-01-28 00:24:38 -0800

    Add program for running lab reactions

---
+++ src/programs/city/labs.js
@@ -0,0 +1,51 @@
+'use strict'
+
+class CityLabs extends kernel.process {
+  constructor (...args) {
+    super(...args)
+    this.priority = PRIORITIES_CITY_LABS
+  }
+
+  getDescriptor () {
+    return this.data.room
+  }
+
+  main () {
+    if (!Game.rooms[this.data.room]) {
+      return this.suicide()
+    }
+    this.room = Game.rooms[this.data.room]
+    if (!this.room.structures[STRUCTURE_LAB] || !this.room.getRoomSetting('LABS')) {
+      return this.suicide()
+    }
+
+    const limit = 1
+    let run = 0
+    const feeders = this.room.getFeederLabs()
+    const vats = this.room.getVatLabs()
+    if (!feeders || !vats) {
+      return
+    }
+    const product = REACTIONS[feeders[0].mineralType][feeders[1].mineralType]
+    for (const vat of vats) {
+      if (vat.cooldown) {
+        continue
+      }
+      if (vat.mineralAmount > 0) {
+        if (vat.mineralType !== product) {
+          continue
+        }
+        if (vat.mineralCapacity - vat.mineralAmount < LAB_REACTION_AMOUNT) {
+          continue
+        }
+      }
+      vat.runReaction(feeders[0], feeders[1])
+      run++
+      if (run >= limit) {
+        break
+      }
+    }
+  }
+}
+
+module.exports = CityLabs
