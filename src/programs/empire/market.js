689170a1d551f04ddabf65fe2b9c728b4f16f5bb
duckymirror <33291860+duckymirror@users.noreply.github.com>
2020-03-13 22:46:24 +0100

    Standard

--- src/programs/empire/market.js
+++ src/programs/empire/market.js
@@ -98,8 +98,9 @@ class EmpireMarket extends kernel.process {
   getResourceTarget (resource) {
     const terminals = _.shuffle(this.terminals)
     for (const terminal of terminals) {
-      if (terminal.store.getFreeCapacity() <= 0)
-        continue;
+      if (terminal.store.getFreeCapacity() <= 0) {
+        continue
+      }
       const room = terminal.room
       if (!terminal.store[resource] || terminal.store[resource] < RESERVE_AMOUNT) {
         if (room.storage.getResourceNeed(resource)) {
